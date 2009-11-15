c79bf2463f02eb15a2f8922df7c2995ec88890e4
jantore <jantore@32k.org>
2009-11-15 13:27:57 +0100

    Fix indentation, convert tabs to spaces.

--- main.js
+++ main.js
@@ -2,22 +2,22 @@ Ext.onReady(function() {
     Ext.QuickTips.init();
 
     var viewport = new Ext.Viewport({
-	layout : 'border',
-	items  : [
-	    new SearchPanel(),
-	    new ListPanel({
-		id : 'list-panel',
-		items : [
-		    new FrontPanel()
-		]
-	    }),
-	    new WestPanel({
-		items : [
-		    new PlaylistGrid(),
-		    new SearchlistGrid(),
-		]
-	    }),
-	    new PlayerPanel({ id : 'player-panel' }),
-	],
+        layout : 'border',
+        items  : [
+            new SearchPanel(),
+            new ListPanel({
+                id : 'list-panel',
+                items : [
+                    new FrontPanel()
+                ]
+            }),
+            new WestPanel({
+                items : [
+                    new PlaylistGrid(),
+                    new SearchlistGrid(),
+                ]
+            }),
+            new PlayerPanel({ id : 'player-panel' }),
+        ],
     });
 });
