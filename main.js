57ef5f613c881621596752213ba5a4660d7f6328
jantore <jantore@32k.org>
2009-11-15 11:11:01 +0100

    Set id in PlayerPanel constructor, fix indentation, remove unused variable definitions.

--- main.js
+++ main.js
@@ -1,36 +1,22 @@
 Ext.onReady(function() {
-  Ext.QuickTips.init();
+    Ext.QuickTips.init();
 
-  var search = new SearchPanel();
-
-  var playlist = new PlaylistGrid();
-  var searchlist = new SearchlistGrid();
-  var west = new WestPanel({
-    items: [
-      playlist,
-      searchlist
-    ],
-  });
-
-  var viewport = new Ext.Viewport({
-    layout : 'border',
-    items  : [
-      new SearchPanel(),
-      new ListPanel({
+    var viewport = new Ext.Viewport({
+	layout : 'border',
+	items  : [
+	    new SearchPanel(),
+	    new ListPanel({
         items : [
-          new FrontPanel()
+            new FrontPanel()
         ]
-      }),
-      new WestPanel({
-        items : [
-          new PlaylistGrid(),
-          new SearchlistGrid(),
-        ]
-      }),
-      new PlayerPanel(),
-    ],
-  });
-
-  playlist.store.load();
-
+	    }),
+	    new WestPanel({
+		items : [
+		    new PlaylistGrid(),
+		    new SearchlistGrid(),
+		]
+	    }),
+	    new PlayerPanel({ id : 'player-panel' }),
+	],
+    });
 });
